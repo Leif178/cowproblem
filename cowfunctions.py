aab2adf6731744be455b9d7c0c98004c28585c90
Aleyna Akyuz <aleyna@Aleyna-MBP-5.lan>
2023-09-09 20:03:04 -0400

    Changes made - Return of the Newpos function is changed - Return of the Energy function is a list now. - Mass is a global variable since it is known. - I used while loop rather than a for loop since simulation needs to stop when the cow hits the ground. We should not specify the time interval. - Plots are added.

--- cowfunctions.py
+++ cowfunctions.py
@@ -1,1 +1,24 @@
-import numpy as npg=9.81def Forces(m,v, k=0):    #This function calculates the instanteneous force on the cow, where the inputs are the mass (m), instanteneous    #velocity (v) and the drag coefficient (k)    return [-np.sign(v[0])*k*v[0]**2,-m*g-np.sign(v[1])*k*v[1]**2]def Energy (m, r, v):    #This function calculates the kinetic energy, potential energy, and total energy and will return them in that order    #The arguments to this function are mass (m), position (r), and velocity (v)    KE=1/2*m*(v[0]**2+v[1]**2)    PE=m*g*r[1]    H=KE+PE    return KE, PE, Hdef Newpos(x,v,dt):    return [x[0]+v[0]*dt,x[1]+v[1]*dt]def Newv(m, v, f, dt):    return [v[0]+f[0]/m*dt,v[1]+f[1]/m*dt]
+import numpy as np
+
+g = 9.81
+m = 1000
+def Forces(v, k=0):
+    #This function calculates the instanteneous force on the cow, where the inputs are instanteneous
+    #velocity (v) and the drag coefficient (k)
+    return [-np.sign(v[0])*k*v[0]**2,-m*g-np.sign(v[1])*k*v[1]**2]
+
+
+def Energy (r, v):
+    #This function calculates the kinetic energy, potential energy, and total energy and will return them in that order
+    #The arguments to this function are position (r), and velocity (v)
+    KE=1/2*m*(v[0]**2+v[1]**2)
+    PE=m*g*r[1]
+    H=KE+PE
+    return [KE, PE, H]
+
+def Newpos(x,v,f,dt):
+    a = [i/m for i in f]
+    return [x[0]+v[0]*dt+(1/2)*a[0]*dt**2,x[1]+v[1]*dt+(1/2)*a[1]*dt**2]
+
+def Newv(v, f, dt):
+    return [v[0]+f[0]/m*dt,v[1]+f[1]/m*dt]
--- cowprob.py
+++ cowprob.py
@@ -1,1 +1,61 @@
-#This is the main filefrom cowproblem.cowfunctions import *import seaborn as snsimport matplotlib as mpimport numpy as np##############################################################################g=9.81 #gravitaional constantk=.1 #drag constantm=1000 # mass in kgpos0=[0, 15] #initial posistion in metersv0=[3,4] #initial position in m/sF0=[0,m*g] # initial force in Nt0=0  #initial time in secondstf= 60 #final time in secondsSteps = 6000    #steps defines the resolution dt=np.ceil((tf - t0)/Steps) #this is the small change in timet=np.arange(t0,tf,dt) #creates a timeseries from t0 to tf with size Steps##############################################################################xlist=[pos0]vlist=[v0]Flist=[F0]for i  in range(len(list(t))-1):    xlist.append(Newpos(xlist[i],vlist[i],dt))    vlist.append(Newv(m,vlist[i],Flist[i],dt))    Flist.append(Forces(m,vlist[i],k))    ##############################################################################
+#This is the main file
+
+from cowfunctions import *
+import matplotlib.pyplot as plt
+
+##############################################################################
+## Initial conditions
+
+g=9.81 #gravitaional constant
+k=.1 #drag constant
+
+m=1000 # mass in kg
+pos0=[0, 15] #initial posistion in meters
+v0=[3,4] #initial position in m/s
+F0=[0,m*g] # initial force in N
+KE0=1/2*m*(v0[0]**2+v0[1]**2) #Initial Kinetic Energy
+PE0=m*g*pos0[1] #Initial Potential Energy
+dt=0.1 #time step
+
+##############################################################################
+## Iteration
+
+xlist=[pos0]
+vlist=[v0]
+Flist=[F0]
+Elist=[[KE0, PE0, KE0+PE0]]
+time=[0]
+
+i=0
+while xlist[i][1]>=0:
+    xlist.append(Newpos(xlist[i],vlist[i],Flist[i],dt))
+    vlist.append(Newv(vlist[i],Flist[i],dt))
+    Flist.append(Forces(vlist[i],k))
+    Elist.append(Energy(xlist[i], vlist[i]))
+    time.append(dt*i)
+    i+=1
+    
+##############################################################################
+## Plotting
+
+x1=[xlist[i][0] for i in range(len(xlist))]
+x2=[xlist[i][1] for i in range(len(xlist))]
+
+plt.plot(x1,x2)
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.title('Trajectory')
+plt.show()
+
+KE = [Elist[i][0] for i in range(len(Elist))]
+PE = [Elist[i][1] for i in range(len(Elist))]
+H = [Elist[i][2] for i in range(len(Elist))]
+
+plt.plot(time, KE, label='Kinetic Energy')
+plt.plot(time, PE, label='Potential Energy')
+plt.plot(time, H, label='Total Energy')
+plt.title('Energy')
+plt.xlabel('time (sec)')
+plt.ylabel('jules')
+plt.legend()
+plt.show()
